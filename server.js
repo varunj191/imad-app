63186a34d432ec7f8515a7af0f6922d94453cbae
varunj191 <varun.j2506@gmail.com>
2017-08-21 00:00:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('Article one restarted');
+});
+app.get('/article-two', function (req, res) {
+ res.send('Article two restarted');
+});
+app.get('article-three', function (req, res) {
+res.send('Article three restarted');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
