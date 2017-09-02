f6028153074a34498e44996bb4f5566d0faca349
varunj191 <varun.j2506@gmail.com>
2017-09-02 23:28:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
-var button = document.getElementById('counter1');
-var counter1 = 0;
+var button = document.getElementById('counter');
+var counter = 0;
 
  button.onclick = function () {
-    counter1 = counter1 + 1 ;
-    var span = document.getElementById('count1');
-    span.innerHTML = counter1.toString();
+    counter = counter + 1 ;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
 };
