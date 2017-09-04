4e8a7e275b5c6c48abba1fbc4047bfefebcf3abc
varunj191 <varun.j2506@gmail.com>
2017-09-05 01:20:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,8 +24,8 @@ var request = new XMLHttpRequest();
 //
 var Input = document.getElementById('inputbox')
 var inputbox = Input.value;
-var sumbit = document.getElementById('Submit1')
-Submit1.onclick = function () {
+var submit = document.getElementById('Submit1')
+submit.onclick = function () {
     var names = ['name1','name2','name3'];
     var list = '';
     for(i=0; i < names.length ; i++) {
