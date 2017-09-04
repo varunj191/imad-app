ed0dbee56591dc76037494984383f2c964459f3c
varunj191 <varun.j2506@gmail.com>
2017-09-05 00:35:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,23 @@ var counter = 0;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
 };
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function () {
+
+var counter = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (Request.readystate === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    } 
+ //button.onclick = function () {
+ //   counter = counter + 1 ;
+ //   var span = document.getElementById('count');
+ //   span.innerHTML = counter.toString();
+};
